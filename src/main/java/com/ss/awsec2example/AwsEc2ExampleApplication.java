4170a8fdf36a153df9d701f3d6fc62cec62da64f
Sominath Shelake <30572097+shelke88@users.noreply.github.com>
2025-08-01 14:03:56 +0530

    Initial commit

---
+++ src/main/java/com/ss/awsec2example/AwsEc2ExampleApplication.java
@@ -0,0 +1,13 @@
+package com.ss.awsec2example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsEc2ExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsEc2ExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ss/awsec2example/AwsEc2ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ss.awsec2example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsEc2ExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
